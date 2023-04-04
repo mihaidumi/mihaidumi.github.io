356e34a4f397e0ac547541ff652583b68305a971
mihaidumi <mfdumitrescu@gmail.com>
2023-04-04 23:04:09 +0300

    Update main.js

--- main.js
+++ main.js
@@ -9,29 +9,29 @@
 
         for(let i = 0; i<=inputs.length; i++){
             if(!option1[i] || !option2[i]) {
-                return document.getElementById("result").innerHTML="Please enter rock, paper or sccisors"
+                return document.getElementById("result").textContent="Please enter rock, paper or sccisors"
             }
         }
         
 
         if (option1.length == 0 || option2.length == 0) {
-            return document.getElementById("result").innerHTML="Please enter rock, paper or sccisors"           
+            return document.getElementById("result").textContent="Please enter rock, paper or sccisors"           
         }
         
         if(option1 === option2) {
-            return document.getElementById("result").innerHTML="Tie game"
+            return document.getElementById("result").textContent="Tie game"
         }else if(option1 === "rock" && option2 === "paper") {
-            document.getElementById("result").innerHTML=`${option2.toUpperCase()} wins` 
+            document.getElementById("result").textContent=`${option2.toUpperCase()} wins` 
         }else if(option1 === "rock" && option2 === "sccisors" ){
-            document.getElementById("result").innerHTML=`${option1.toUpperCase()} wins`
+            document.getElementById("result").textContentL=`${option1.toUpperCase()} wins`
         }else if(option1 === "sccisors" && option2 === "rock"){
-            document.getElementById("result").innerHTML=`${option2.toUpperCase()} wins`
+            document.getElementById("result").textContent=`${option2.toUpperCase()} wins`
         }else if(option1 === "sccisors" && option2 === "paper"){
-            document.getElementById("result").innerHTML=`${option1.toUpperCase()} wins`
+            document.getElementById("result").textContent=`${option1.toUpperCase()} wins`
         }else if(option1 === "paper" && option2 === "rock"){
-            document.getElementById("result").innerHTML=`${option1.toUpperCase()} wins`
+            document.getElementById("result").textContent=`${option1.toUpperCase()} wins`
         }else if(option1 === "paper" && option2 === "sccisors"){
-            document.getElementById("result").innerHTML=`${option2.toUpperCase()} wins`;
+            document.getElementById("result").textContent=`${option2.toUpperCase()} wins`;
         }            
 
     }
