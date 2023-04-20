7eb5000815694afd06f5f4229c5174442978bd30
mihaidumi <mfdumitrescu@gmail.com>
2023-04-20 22:31:25 +0300

    Update index.js
    Using create element

--- index.js
+++ index.js
@@ -4,41 +4,65 @@ const addItem = document.getElementById("add-item");
 const getItem = document.getElementById("input-item");
 const outputItem = document.getElementById("output");
 let error = document.getElementById("error");
+
 let shoppingItems = [];
 let listItems = "";
+
+
 
 
 addItem.onclick = item;
 
 
+
 function item(e){
     e.preventDefault();
     if(!getItem.value){
-        return error.textContent = "Please enter a valid shopping item."
+        return error.textContent = "You must write something."
     }else{
         shoppingItems.push(getItem.value); 
         renderItems();
     }           
 }
 
-function renderItems(){
+function renderItems(){    
     if(!getItem.value){
-        return error.textContent = "Please enter a valid shopping item."
-    }else{        
-    for(let i = 0; i < shoppingItems.length; i += 1){
-        listItems += "<li>" + shoppingItems[i] + "</li>"                          
-    }    
-    error.textContent = "";          
-    outputItem.innerHTML = listItems;    
-    listItems = "";    
-    }       
+        return error.textContent = "You must write something."
+    }else{  
+        let li = document.createElement("li")
+        li.classList.add("list-item")
+        li.innerHTML =  getItem.value 
+        outputItem.appendChild(li)
+        let removeBtn = document.createElement("button")
+        removeBtn.textContent="X"
+        removeBtn.classList.add("remove-btn")
+        li.appendChild(removeBtn)            
+    //for(let i = 0; i < shoppingItems.length; i += 1){        
+        //listItems += li + shoppingItems[i] + "<button id='remove-btn' onClick=removeItem()>X</button>"             
+    //}         
+        error.textContent = ""              
+        //outputItem.innerHTML = listItems
+        console.log(listItems)   
+        listItems = "";    
+    }
+    error.textContent = "" 
+    getItem.value = ""  
+         
+}
+
+function removeItem(e){
+    console.log(e.target)
+    
+}
+
+function boughtItem(){
+    listItems.style.textDecoration = "line-through";
 }
 
 function clearList(){    
     listItems = ""
     outputItem.innerHTML = ""
-    shoppingItems = [] 
-    renderItems()
+    shoppingItems = []    
     error.textContent = ""     
 }
 
