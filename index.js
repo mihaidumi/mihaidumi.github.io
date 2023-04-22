afa6dfe78b5da0aec838c83c89fbfe00fff43876
mihaidumi <mfdumitrescu@gmail.com>
2023-04-22 14:14:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 
 
+
+
 const addItem = document.getElementById("add-item");
 const getItem = document.getElementById("input-item");
 const outputItem = document.getElementById("output");
 let error = document.getElementById("error");
-
 let shoppingItems = [];
-let listItems = "";
+
+
+
+
 
 addItem.onclick = item;
+
+
 
 function item(e){
     e.preventDefault();
@@ -24,32 +30,25 @@ function renderItems(){
     if(!getItem.value){
         return error.textContent = "You must write something."
     }else{  
-        let li = document.createElement("li")
+        /*let li = document.createElement("li")
         li.classList.add("list-item")
         li.innerHTML =  getItem.value 
         outputItem.appendChild(li)
         let removeBtn = document.createElement("button")
         removeBtn.textContent="X"
         removeBtn.classList.add("remove-btn")
-        li.appendChild(removeBtn)            
-    //for(let i = 0; i < shoppingItems.length; i += 1){        
-        //listItems += li + shoppingItems[i] + "<button id='remove-btn' onClick=removeItem()>X</button>"             
-    //}         
+        li.appendChild(removeBtn)*/ 
+        let listItems = "";          
+        for(let i = 0; i < shoppingItems.length; i += 1){        
+            listItems += "<li>" + shoppingItems[i] + "<button id='remove-btn' onClick=removeItem()>X</button>" + "</li>"             
+    }         
         error.textContent = ""              
-        //outputItem.innerHTML = listItems
-        console.log(listItems)   
-        listItems = "";    
+        outputItem.innerHTML = listItems
+        console.log(listItems)          
     }
-    error.textContent = "" 
-    getItem.value = ""  
-         
+    error.textContent = ""    
+    getItem.value = ""          
 }
-
-
-
-
-
-
 
 
 function clearList(){    
@@ -58,6 +57,9 @@ function clearList(){
     shoppingItems = []    
     error.textContent = ""     
 }
+
+    
+
 
     
 
